baa63650c79939edb73e340c19f245f735764f50
monojithalder <monojithalder@hotmail.com>
2018-01-14 11:51:43 +0530

    Change code in serial write to self recover from error

--- HomeAutomationServer.py
+++ HomeAutomationServer.py
@@ -6,7 +6,9 @@ import time
 import sys
 import glob
 
+ser = ''
 class GP(BaseHTTPRequestHandler):
+    global ser
     ports = glob.glob('/dev/tty[A-Za-z]*')
     result = []
     for port in ports:
@@ -31,16 +33,33 @@ class GP(BaseHTTPRequestHandler):
         print parse_qs(self.path[2:])
         self.wfile.write("<html><body><h1>Get Request Received!</h1></body></html>")
     def do_POST(self):
+        global ser
         self._set_headers()
         form = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
             environ={'REQUEST_METHOD': 'POST'}
         )
-        print form.getvalue("foo")
+        #print form.getvalue("foo")
         pin_no = form.getvalue("pinNo")
-        self.ser.write(pin_no)
-        self.wfile.write('{"success": 1}')
+        try:
+            ser.write(pin_no)
+            self.wfile.write('{"success": 1}')
+        except:
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+            result = []
+            for port in ports:
+                try:
+                    s = serial.Serial(port)
+                    s.close()
+                    result.append(port)
+                except (OSError, serial.SerialException):
+                    pass
+            port = result[0]
+            ser = serial.Serial(port, 9600)
+            time.sleep(10)
+            self.wfile.write('{"success": 0}')
+            #pass
         
 
 def run(server_class=HTTPServer, handler_class=GP, port=8088):
